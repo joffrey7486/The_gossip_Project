beee67f2279222f6207fbf9ad1c29409ccb7cc8c
joffrey <joffreycaugnon@gmail.com>
2021-11-01 19:33:31 +0100

    fin de jour 26

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,12 +1,23 @@
 class StaticController < ApplicationController
   def home
     @all_gossips = Gossip.all
-    
   end
 
   def team
   end
 
   def contact
+  end
+
+  def client_show
+    @client = params[:client]
+  end
+
+  def gossip_show
+    @gossip = Gossip.find_by(id: params[:id])
+  end
+
+  def user_show
+    @user = User.find_by(id: params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'acceuil', to: 'static#home'
+  get 'accueil/', to: 'static#home'
   get 'team', to: 'static#team'
   get 'contact', to: 'static#contact'
+  get 'accueil/:client', to: 'static#client_show'
+  get 'gossips/:id', to: "static#gossip_show"
+  get 'users/:id', to: 'static#user_show'
 end
